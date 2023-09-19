26238e2304400fef9038046090639692c762dfc0
Jose Pavon <RannerJP@gmail.com>
2023-09-19 15:07:15 -0700

    added another mapping

--- Application/Application/src/main/java/GameRecommendationSystem/Application/Controller.java
+++ Application/Application/src/main/java/GameRecommendationSystem/Application/Controller.java
@@ -5,13 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 	@GetMapping("/hi")
-	public String HelloWorld(){
+	public String helloWorld(){
 	return "Hi everyone, this is Group-Name (Best group name ever). ";
 	}
 
 	@GetMapping("/purpose")
-	public String HelloWorld(){
+	public String purpose(){
 		String purpose = "The purpose of this project is to create a game recommendation system.";
 	return purpose;
 	}
+
+	@GetMapping("/")
+	public String aboutMe(){
+		return "Hi I am the controller weeeeeee";
+	}
+
 }
