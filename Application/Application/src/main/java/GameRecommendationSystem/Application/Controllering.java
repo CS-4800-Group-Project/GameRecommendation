6da5cf089b97398cc8c5b419a5096fbef290bafd
BenLeCPP <89365181+BenLeCPP@users.noreply.github.com>
2023-09-22 18:53:12 -0700

    fixed homepage.html + controller file

--- Application/Application/src/main/java/GameRecommendationSystem/Application/Controllering.java
+++ Application/Application/src/main/java/GameRecommendationSystem/Application/Controllering.java
@@ -1,22 +1,23 @@
 package GameRecommendationSystem.Application;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class Controller {
+@Controller
+public class Controllering {
 	@GetMapping("/hi")
 	public String helloWorld(){
 		return "Hi everyone, this is Group-Name (Best group name ever). ";
 	}
 
-	@GetMapping("/homepage")
-	public String purpose(){
-		return "homepage.html";
-	}
-
-	@GetMapping("/")
+	@GetMapping("/Self")
 	public String aboutMe(){
 		return "Hi I am the controller weeeeeee";
 	}
+	
+    @GetMapping("/")
+    public String welcome() {
+        return "homepage";
+    }
 
 }
+
