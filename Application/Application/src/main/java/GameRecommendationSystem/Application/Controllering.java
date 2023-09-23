2741d7e877c96d734448c8ba34aeef5d16524e57
Jose Pavon <RannerJP@gmail.com>
2023-09-22 19:54:47 -0700

    Proof of concept for JSON parser

--- Application/Application/src/main/java/GameRecommendationSystem/Application/Controllering.java
+++ Application/Application/src/main/java/GameRecommendationSystem/Application/Controllering.java
@@ -1,9 +1,19 @@
 package GameRecommendationSystem.Application;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @Controller
 public class Controllering {
+	private String jsonString = "{\"name\": \"Jose\", \"age\": 20}";
+	ObjectMapper objectMapper = new ObjectMapper();
+
 	@GetMapping("/hi")
 	public String helloWorld(){
 		return "Hi everyone, this is Group-Name (Best group name ever). ";
@@ -17,6 +27,24 @@ public class Controllering {
     @GetMapping("/")
     public String welcome() {
         return "homepage";
+    }
+	
+	@GetMapping("/JSON")
+    public String json(Model model) {
+		String name = null;
+		String age = null;
+		try {
+			JsonNode jsonNode = objectMapper.readTree(jsonString);
+			name = jsonNode.get("name").asText();
+			age = jsonNode.get("age").asText();
+
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+        return "name";
     }
 
 }
