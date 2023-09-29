ed8f53c09fbd06ef15def1efe5c939889c4ccc92
Jose Pavon <RannerJP@gmail.com>
2023-09-29 11:53:03 -0700

    change back to 8080 port (mine didnt work)

--- Application/Application/src/test/java/GameRecommendationSystem/Application/ApplicationTests.java
+++ Application/Application/src/test/java/GameRecommendationSystem/Application/ApplicationTests.java
@@ -1,13 +1,29 @@
 package GameRecommendationSystem.Application;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+@ContextConfiguration
+@SpringBootTest(classes = {Application.class}, webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application.properties")
 class ApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTest;
 
 	@Test
-	void contextLoads() {
+	void onWebLoad() {
+		String response = restTest.getForObject("http://localhost:" + port + "/", String.class);
+		assertThat(response).contains("VIDEO GAME");
 	}
 
 }
