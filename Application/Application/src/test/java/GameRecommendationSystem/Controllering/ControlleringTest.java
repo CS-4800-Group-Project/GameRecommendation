859ce2f68bb8a2b7eb8a9fc5a48654add61687a7
BenLeCPP <89365181+BenLeCPP@users.noreply.github.com>
2023-09-29 11:45:49 -0700

    Added JUnit test for JSoup

--- Application/Application/src/test/java/GameRecommendationSystem/Controllering/ControlleringTest.java
+++ Application/Application/src/test/java/GameRecommendationSystem/Controllering/ControlleringTest.java
@@ -1,5 +1,33 @@
 package GameRecommendationSystem.Controllering;
-import org.junit.*;
-public class ControlleringTest {
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import GameRecommendationSystem.Application.Application;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+@ContextConfiguration
+@SpringBootTest(classes = {Application.class}, webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application.properties")
+public class ControlleringTest {
+	
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/Jsoup",
+			String.class)).contains("JSoup");
+		}
+	
 }
