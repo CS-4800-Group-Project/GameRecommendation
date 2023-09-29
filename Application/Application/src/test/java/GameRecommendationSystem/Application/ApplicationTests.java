48f2b560cab60a7ebc195406dc86d41f3d5dff88
LynnTaka <tigerlynn11@gmail.com>
2023-09-29 11:25:51 -0700

    controller creation test

--- Application/Application/src/test/java/GameRecommendationSystem/Application/ApplicationTests.java
+++ Application/Application/src/test/java/GameRecommendationSystem/Application/ApplicationTests.java
@@ -1,13 +1,22 @@
 package GameRecommendationSystem.Application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+public class ApplicationTests {
+
+	// check to make sure controller is being created
+	@Autowired
+	private Controllering controller;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
