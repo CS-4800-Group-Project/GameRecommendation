e1f173a7c83775a704ab8ae72c007a27594fa07d
BenLeCPP <89365181+BenLeCPP@users.noreply.github.com>
2023-10-06 17:22:57 -0700

    Added new methods for api

--- Application/Application/src/main/java/GameRecommendationSystem/Application/GameRecommendationController.java
+++ Application/Application/src/main/java/GameRecommendationSystem/Application/GameRecommendationController.java
@@ -1,73 +1,115 @@
 package GameRecommendationSystem.Application;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+
 @Controller
 public class GameRecommendationController {
-
+	private List<String> titles = new ArrayList <>();
     private final RestTemplate restTemplate;
-    private final String mobyGamesBaseUrl = "https://api.mobygames.com/v1"; 
+    private final String mobyGamesBaseUrl = "https://api.mobygames.com/v1/"; 
     public final static String apiKey = "moby_kZCBUgn5Hbs4CYbFhKpYTnvALrr"; 
-
-    public GameRecommendationController(RestTemplate restTemplate) {
+    private ObjectMapper objectMapper;
+    
+    @Autowired
+    public GameRecommendationController(RestTemplate restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+  
     }
-
-    @GetMapping("/genres")
-    public ResponseEntity<String> getGenres() {
-        String apiGenresUrl = mobyGamesBaseUrl + "/genres?api_key=" + apiKey;
-
+    
+    @GetMapping("/gameTitle")
+    public ResponseEntity<List<String>> getGame(@RequestParam(defaultValue = "0") int offset) {
+        int batchSize = 100; 
         try {
-            ResponseEntity<String> genresResponse = restTemplate.exchange(apiGenresUrl, HttpMethod.GET, null, String.class);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
-            return ResponseEntity.ok(genresResponse.getBody());
+            String apiUrl = mobyGamesBaseUrl +"games?api_key=" + apiKey + "&offset=" + offset + "&limit=" + batchSize;
+
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<JsonNode> gamesResponse = restTemplate.exchange(
+                    apiUrl,
+                    HttpMethod.GET,
+                    entity,
+                    JsonNode.class
+            );
+
+           
+            JsonNode gamesNode = gamesResponse.getBody();
+            for (JsonNode game : gamesNode.get("games")) {
+                String title = game.get("title").asText();
+                titles.add(title);
+            }
+
+            return ResponseEntity.ok(titles);
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error fetching genres");
-        }
-    }
-    @GetMapping("/groups")
-    public ResponseEntity<String> getGroups() {
-        String apiGenresUrl = mobyGamesBaseUrl + "/groups?api_key=" + apiKey;
-
-        try {
-            ResponseEntity<String> groupsResponse = restTemplate.exchange(apiGenresUrl, HttpMethod.GET, null, String.class);
-
-            return ResponseEntity.ok(groupsResponse.getBody());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error fetching groups");
-        }
-    }
-    @GetMapping("/games")
-    public ResponseEntity<String> getGames() {
-        String apiGenresUrl = mobyGamesBaseUrl + "/games?api_key=" + apiKey;
-
-        try {
-            ResponseEntity<String> gamesResponse = restTemplate.exchange(apiGenresUrl, HttpMethod.GET, null, String.class);
-
-            return ResponseEntity.ok(gamesResponse.getBody());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error fetching games");
-        }
-    }
-    @GetMapping("/games/{id}")
-    public ResponseEntity<String> getGamesWithID(@PathVariable String id) {
-        String apiGenresUrl = mobyGamesBaseUrl + "/games?id=" + id + "&api_key=" + apiKey;
-
-        try {
-         
-            ResponseEntity<String> gamesResponse = restTemplate.exchange(apiGenresUrl, HttpMethod.GET, null, String.class);
-
-            
-            return ResponseEntity.ok(gamesResponse.getBody());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error fetching games");
+            return ResponseEntity.status(500).body(Collections.singletonList("Error fetching games"));
         }
     }
     
+    @GetMapping("/games")
+    public ResponseEntity<JsonNode> getGames() {
+        String apiUrl = "https://api.mobygames.com/v1/games?api_key=" + apiKey;
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<JsonNode> gamesResponse = restTemplate.exchange(
+                    apiUrl,
+                    HttpMethod.GET,
+                    entity,
+                    JsonNode.class
+            );
+
+            return ResponseEntity.ok(gamesResponse.getBody());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(objectMapper.createObjectNode().put("error", "Error fetching games"));
+        }
+    } 
+
+    
+   
 }
+
+    
+
+
+   
+
+
+
+
+
+
+
+
+
+
+  
+   
+
+
+
+
 
 	
 	
