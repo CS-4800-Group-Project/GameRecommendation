e1c0e9b87691f831254144a8102bd2ddef4824b9
LynnTaka <tigerlynn11@gmail.com>
2023-10-20 16:44:33 -0700

    changes to GameRec so no longer have to use api

--- Application/Application/src/main/java/GameRecommendationSystem/Application/GameRecommendationController.java
+++ Application/Application/src/main/java/GameRecommendationSystem/Application/GameRecommendationController.java
@@ -1,199 +1,247 @@
 package GameRecommendationSystem.Application;
 
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import org.springframework.web.client.RestTemplate;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 import javax.annotation.PostConstruct;
+import java.io.*;
+import java.util.*;
+import java.text.*;
 
 
-import org.springframework.http.HttpHeaders;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
 
 @Controller
 public class GameRecommendationController {
     private final RestTemplate restTemplate;
-    private final String mobyGamesBaseUrl = "https://api.mobygames.com/v1/"; 
-    public final static String apiKey = "moby_kZCBUgn5Hbs4CYbFhKpYTnvALrr"; 
+    private final String mobyGamesBaseUrl = "https://api.mobygames.com/v1/";
+    public final static String apiKey = "moby_kZCBUgn5Hbs4CYbFhKpYTnvALrr";
     private ObjectMapper objectMapper;
-    List<Game> gamesList = new ArrayList<>();
-    
+    private List<Game> gamesList = new ArrayList<>();
 
+
+    @Value("${game.data.file.path}")
+    private String gameDataFilePath;
+   
     @Autowired
-    public GameRecommendationController(RestTemplate restTemplate,ObjectMapper objectMapper) {
+    public GameRecommendationController(RestTemplate restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
         this.objectMapper = objectMapper;
     }
 
+
     private final int gamesPerPage = 100; // Number of games to fetch per request
     private final long requestDelayMillis = 1000; // Delay between requests in milliseconds (1 second)
-    private final int maxGamesToFetch = 30000;
+    private final int maxGamesToFetch = 800;
+
+
     @PostConstruct
     public void loadGameList() {
         try {
-            int offset = 0; // Offset for pagination
-            int totalGamesFetched = 0; // Keep track of the total games fetched
-
-            while (totalGamesFetched < maxGamesToFetch) {
-                String apiUrl = "https://api.mobygames.com/v1/games?api_key=" + apiKey + "&offset=" + offset;
-
-                HttpHeaders headers = new HttpHeaders();
-                headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-                HttpEntity<?> entity = new HttpEntity<>(headers);
-
-                ResponseEntity<JsonNode> gamesResponse = restTemplate.exchange(
-                        apiUrl,
-                        HttpMethod.GET,
-                        entity,
-                        JsonNode.class
-                );
-
-                if (gamesResponse.getStatusCode().is4xxClientError() || gamesResponse.getStatusCode().is5xxServerError()) {
-                    // Handle rate limiting or other errors
-                    System.out.println("Error: " + gamesResponse.getStatusCode());
-                    break;
+           
+            loadGameDataFromFile();
+            if (gamesList.isEmpty()) {
+                // If there are no games in game_data.json, fetch games from the API
+                int offset = 0; // Offset for pagination
+                int totalGamesFetched = 0; // Keep track of the total games fetched
+                System.out.println(offset);
+                while (totalGamesFetched < maxGamesToFetch) {
+                    String apiUrl = "https://api.mobygames.com/v1/games?api_key=" + apiKey + "&offset=" + offset;
+   
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+   
+                    HttpEntity<?> entity = new HttpEntity<>(headers);
+   
+                    ResponseEntity<JsonNode> gamesResponse = restTemplate.exchange(
+                            apiUrl,
+                            HttpMethod.GET,
+                            entity,
+                            JsonNode.class
+                    );
+   
+                    if (gamesResponse.getStatusCode().is4xxClientError() || gamesResponse.getStatusCode().is5xxServerError()) {
+                        // Handle rate limiting or other errors
+                        System.out.println("Error: " + gamesResponse.getStatusCode());
+                        break;
+                    }
+   
+                    // Get the games from the response and add them to the gamesList
+                    List<Game> games = convertJsonToGameList(gamesResponse.getBody());
+                    gamesList.addAll(games);
+                   
+                    // Update the total games fetched
+                    totalGamesFetched += games.size();
+   
+                    // If the response doesn't contain the expected number of games or the limit is reached, break the loop
+                    if (games.size() < gamesPerPage || totalGamesFetched >= maxGamesToFetch) {
+                        break;
+                    }
+   
+                    // Increment the offset for the next page
+                    offset += gamesPerPage;
+   
+                    // Introduce a 1-second delay to avoid rate limiting
+                    Thread.sleep(requestDelayMillis);
+                   
                 }
-
-                // Get the games from the response and add them to the gamesList
-                List<Game> games = convertJsonToGameList(gamesResponse.getBody());
-                gamesList.addAll(games);
-
-                // Update the total games fetched
-                totalGamesFetched += games.size();
-
-                // If the response doesn't contain the expected number of games or the limit is reached, break the loop
-                if (games.size() < gamesPerPage || totalGamesFetched >= maxGamesToFetch) {
-                    break;
-                }
-
-                // Increment the offset for the next page
-                offset += gamesPerPage;
-
-                // Introduce a 1-second delay to avoid rate limiting
-                Thread.sleep(requestDelayMillis);
+            saveGameDataToFile();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+   
 
 
-    @GetMapping("/game")
-    public ResponseEntity<JsonNode> getGames() {
-        String apiUrl = "https://api.mobygames.com/v1/games?api_key=" + apiKey;
-
+    private void saveGameDataToFile() {
         try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT); // For pretty-printing
 
-            HttpEntity<?> entity = new HttpEntity<>(headers);
 
-            ResponseEntity<JsonNode> gamesResponse = restTemplate.exchange(
-                    apiUrl,
-                    HttpMethod.GET,
-                    entity,
-                    JsonNode.class
-            );
+            // Specify the path to your JSON file
+            File file = new File(gameDataFilePath);
+           
+            if (!file.exists()) {
+                file.createNewFile();
+            }
 
-            // added 
-            gamesList = convertJsonToGameList(gamesResponse.getBody());
 
-            return ResponseEntity.ok(gamesResponse.getBody());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body(objectMapper.createObjectNode().put("error", "Error fetching games"));
+            objectMapper.writeValue(file, gamesList);
+            System.out.println("gamesList has been successfully saved to game_data.json.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Failed to save game data to game_data.json: " + e.getMessage());
         }
-
     }
 
-    // added
 
+    private void loadGameDataFromFile() {
+        try {
+            File file = new File(gameDataFilePath);
+
+
+            if (file.exists()) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                gamesList = objectMapper.readValue(file, new TypeReference<List<Game>>() {});
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Failed to load game data from game_data.json: " + e.getMessage());
+        }
+    }
+   
     private List<Game> convertJsonToGameList(JsonNode jsonNode) {
-        //return 
         List<Game> games = new ArrayList<>();
         PropertyNamingStrategy strategy = objectMapper.getPropertyNamingStrategy();
 
+
         try {
-            // Set the naming strategy to SNAKE_CASE temporarily for this deserialization
-            // double checked format and it is in snake_case.
             objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
 
+
             if (jsonNode.isObject() && jsonNode.has("games")) {
-                JsonNode gamesNode = jsonNode.get("games"); // Get the "games" field
-            
-                // inside games there should be an array of results
+                JsonNode gamesNode = jsonNode.get("games");
+
+
                 if (gamesNode.isArray()) {
-                    System.out.println("INSIDE");//never prints
                     for (JsonNode gameNode : gamesNode) {
                         Game game = objectMapper.convertValue(gameNode, Game.class);
                         games.add(game);
                         System.out.println(game.getTitle() + " " + game.getMobyScore());
                     }
-                }
-                else
-                {
+                } else {
                     System.out.println("ERROR SOMEWHERE");
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            // Reset the original property naming strategy
             objectMapper.setPropertyNamingStrategy(strategy);
         }
         return games;
     }
 
+
     // added
-    // 
+    //
     @GetMapping("/gamesresults")
     public String getGames(Model model) {
         // check to see if any games are in list
         System.out.println("Number of games retrieved: " + gamesList.size());
-        
+       
+
 
         model.addAttribute("games", gamesList); // Add the list of games to the model
 
-        return "results"; // Return the name of the HTML template
+
+        return "searchResults"; // Return the name of the HTML template
     }
-    
+   
     @GetMapping("/search")
     public String searchGame(@RequestParam(name = "title") String title, Model model) {
         // Create a list to store matching games
         List<Game> matchingGames = new ArrayList<>();
 
-        // Search for games with matching titles
+
+        // Normalize the search title for comparison
+        String normalizedTitle = normalizeTitle(title);
+
+
+        // Create a Collator instance for accent-insensitive comparison
+        Collator collator = Collator.getInstance(new Locale("en", "US"));
+
+
         for (Game game : gamesList) {
-            if (game.getTitle().equalsIgnoreCase(title)) {
-                matchingGames.add(game);
-                matchingGames.add(game);
+            String gameTitle = normalizeTitle(game.getTitle());
+
+
+            // Use the Collator instance to perform accent-insensitive comparison
+            if (collator.compare(normalizedTitle, gameTitle) == 0|| gameTitle.contains(normalizedTitle)) {
                 matchingGames.add(game);
             }
         }
 
+
         // Add the matching games to the model
         model.addAttribute("games", matchingGames);
+
 
         // Return the results template
         return "searchResults";
     }
+
+
+    private String normalizeTitle(String title) {
+        // Normalize the title to lowercase and remove diacritical marks (accents)
+        return Normalizer.normalize(title, Normalizer.Form.NFD)
+                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
+                .toLowerCase();
+    }
+
+
+
+
 }
+
+
+
+
+
