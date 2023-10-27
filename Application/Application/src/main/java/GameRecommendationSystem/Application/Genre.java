b671e17bd517f495705f25eea447952aa33f093e
LynnTaka <tigerlynn11@gmail.com>
2023-10-27 13:27:29 -0700

    more getters and setters added genres

--- Application/Application/src/main/java/GameRecommendationSystem/Application/Genre.java
+++ Application/Application/src/main/java/GameRecommendationSystem/Application/Genre.java
@@ -10,6 +10,10 @@ public class Genre {
 
     private String genre_name;
 
+    private String genre_category;
+
+    private Long genre_category_id;
+
     public Long getGenreId() {
         return genre_id;
     }
@@ -18,12 +22,28 @@ public class Genre {
         this.genre_id = genre_id;
     }
 
+    public Long getGenreCategoryId() {
+        return genre_category_id;
+    }
+
+    public void setGenreCategoryId(Long genre_category_id) {
+        this.genre_category_id = genre_category_id;
+    }
+
     public String getGenreName() {
         return genre_name;
     }
 
     public void setGenreName(String genre_name) {
         this.genre_name = genre_name;
+    }
+
+    public String getGenreCategory() {
+        return genre_category;
+    }
+
+    public void setGenreCategory(String genre_category) {
+        this.genre_category = genre_category;
     }
 
    
