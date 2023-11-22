236296e0b756c1b6f2c07c7de0e433eaafeb29f2
LynnTaka <tigerlynn11@gmail.com>
2023-11-22 14:33:12 -0800

    better search results

--- Application/Application/src/main/java/GameRecommendationSystem/Application/GameController.java
+++ Application/Application/src/main/java/GameRecommendationSystem/Application/GameController.java
@@ -39,25 +39,47 @@ public class GameController {
     @GetMapping("/search")
     public String searchGame(@RequestParam(name = "title") String title, Model model) {
         int perspective = -1;
-        int genre = -1;
+        int genre1 = -1;
+        int genre2 = -1;
         int gameplay = -1;
+        int setting = -1;
 
         List<Game> gameList = gameRepository.findByTitleIgnoreCaseContainingRegex(title);
 
         Game matchedGame = gameList.get(0);
 
+        int count = 0;
         for (int i = 0; i < matchedGame.getGenres().size(); i++) {
-            if (matchedGame.getGenres().get(i).getGenreCategoryId() == 1 && genre == -1)
-                genre = matchedGame.getGenres().get(i).getGenreId();
-            else if (matchedGame.getGenres().get(i).getGenreCategoryId() == 2 && perspective == -1) {
+            if (matchedGame.getGenres().get(i).getGenreCategoryId() == 1 && genre1 == -1) {
+                genre1 = matchedGame.getGenres().get(i).getGenreId();
+                count++;
+            } else if (matchedGame.getGenres().get(i).getGenreCategoryId() == 1 && genre2 == -1) {
+                genre2 = matchedGame.getGenres().get(i).getGenreId();
+                count++;
+            } else if (matchedGame.getGenres().get(i).getGenreCategoryId() == 2 && perspective == -1) {
                 perspective = matchedGame.getGenres().get(i).getGenreId();
-            } else if (matchedGame.getGenres().get(i).getGenreCategoryId() == 4 && gameplay == -1)
+                count++;
+            } else if (matchedGame.getGenres().get(i).getGenreCategoryId() == 4 && gameplay == -1) {
                 gameplay = matchedGame.getGenres().get(i).getGenreId();
+                count++;
+            } else if (matchedGame.getGenres().get(i).getGenreCategoryId() == 10 && setting == -1) {
+                setting = matchedGame.getGenres().get(i).getGenreId();
+                count++;
+            }
+
+            if (count == 5)
+                break;
         }
 
-        if (gameplay != -1 && genre != -1 && perspective != -1) {
-            System.out.println("\ngameplay\n");
-            gameList.addAll(gameRepository.findGamesByBothConditions(2, perspective, 1, genre, 4, gameplay));
+        if (gameplay != -1 && genre2 == -1 && genre1 != -1 && perspective != -1 && setting != -1) {
+            System.out.println("\n\n3\n\n");
+            gameList.addAll(
+                    gameRepository.findGamesByFourConditions(2, perspective, 1, genre1, 4, gameplay, 10, setting));
+        } else if (gameplay != -1 && genre2 != -1 && genre1 != -1 && perspective != -1 && setting != -1) {
+            System.out.println("\n\n4\n\n");
+            gameList.addAll(
+                    gameRepository.findGamesByFiveConditions(2, perspective, 1, genre1, 4, gameplay, 1, genre2, 10,
+                            setting));
         }
 
         // else if (gameplay != -1 && genre != -1 && perspective != -1)
--- Application/Application/src/main/java/GameRecommendationSystem/Application/GameRepository.java
+++ Application/Application/src/main/java/GameRecommendationSystem/Application/GameRepository.java
@@ -7,22 +7,31 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface GameRepository extends MongoRepository<Game, String> {
-    List<Game> findByGameId(int gameId);
+        List<Game> findByGameId(int gameId);
 
-    @Query("{'title': {$regex: ?0, $options: 'i'}}")
-    List<Game> findByTitleIgnoreCaseContainingRegex(String title);
+        @Query("{'title': {$regex: ?0, $options: 'i'}}")
+        List<Game> findByTitleIgnoreCaseContainingRegex(String title);
 
-    @Query("{ 'platforms.platformName' : { $in: ?0 } }")
-    List<Game> findByPlatforms(@Param("platforms") List<String> platformNames);
+        @Query("{ 'platforms.platformName' : { $in: ?0 } }")
+        List<Game> findByPlatforms(@Param("platforms") List<String> platformNames);
 
-    @Query("{ 'genres.genreCategory' : { $in: ?0 } }")
-    List<Game> findByGenreCategory(@Param("genres") List<String> genreCategory);
+        @Query("{ 'genres.genreCategory' : { $in: ?0 } }")
+        List<Game> findByGenreCategory(@Param("genres") List<String> genreCategory);
 
-    @Query("{ 'genres.genreCategoryId' : ?0, 'genres.genreId' : ?1 }")
-    List<Game> findByGenreCategoryIdAndGenreId(@Param("genreCategoryId") int genreCategoryId,
-            @Param("genreId") int genreId);
+        @Query("{ 'genres.genreCategoryId' : ?0, 'genres.genreId' : ?1 }")
+        List<Game> findByGenreCategoryIdAndGenreId(@Param("genreCategoryId") int genreCategoryId,
+                        @Param("genreId") int genreId);
 
-    @Query("{ $and: [ { 'genres.genreCategoryId': ?0, 'genres.genreId': ?1 }, { 'genres.genreCategoryId': ?2, 'genres.genreId': ?3 }, { 'genres.genreCategoryId': ?4, 'genres.genreId': ?5 } ] }")
-    List<Game> findGamesByBothConditions(int categoryId1, int genreId1, int categoryId2, int genreId2, int categoryId3,
-            int genreId3);
+        @Query("{ $and: [ { 'genres.genreCategoryId': ?0, 'genres.genreId': ?1 }, { 'genres.genreCategoryId': ?2, 'genres.genreId': ?3 }, { 'genres.genreCategoryId': ?4, 'genres.genreId': ?5 } ] }")
+        List<Game> findGamesByThreeConditions(int categoryId1, int genreId1, int categoryId2, int genreId2,
+                        int categoryId3, int genreId3);
+
+        @Query("{ $and: [ { 'genres.genreCategoryId': ?0, 'genres.genreId': ?1 }, { 'genres.genreCategoryId': ?2, 'genres.genreId': ?3 }, { 'genres.genreCategoryId': ?4, 'genres.genreId': ?5 }, { 'genres.genreCategoryId': ?6, 'genres.genreId': ?7 } ] }")
+        List<Game> findGamesByFourConditions(int categoryId1, int genreId1, int categoryId2, int genreId2,
+                        int categoryId3, int genreId3, int categoryId4, int genreId4);
+
+        @Query("{ $and: [ { 'genres.genreCategoryId': ?0, 'genres.genreId': ?1 }, { 'genres.genreCategoryId': ?2, 'genres.genreId': ?3 }, { 'genres.genreCategoryId': ?4, 'genres.genreId': ?5 }, { 'genres.genreCategoryId': ?6, 'genres.genreId': ?7 }, { 'genres.genreCategoryId': ?8, 'genres.genreId': ?9 } ] }")
+        List<Game> findGamesByFiveConditions(int categoryId1, int genreId1, int categoryId2, int genreId2,
+                        int categoryId3, int genreId3, int categoryId4, int genreId4, int categoryId5, int genreId5);
+
 }
